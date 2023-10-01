5102be38f1dc694f7acb19b9acd670490e87072b
Swagom@123 <78723890+Salucas237t25273@users.noreply.github.com>
2023-10-01 18:05:51 +0530

    Update collegedb.sql

--- DB/collegedb.sql
+++ DB/collegedb.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 26, 2023 at 06:48 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+/*SQL Script*/
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
